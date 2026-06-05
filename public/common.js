058ccabfc2ae994fcdf80f92cc9586080edd8072
ishikawa shunsuke <syantako@gmail.com>
2026-06-05 13:31:56 +0900

    Update common.js

--- public/common.js
+++ public/common.js
@@ -14,7 +14,7 @@ toggler.addEventListener('blur', () => {
 });
 
 toggler.addEventListener('click', () => {
-    const expanded = toggler.getAttribute('aria-expanded');
+    const expanded = toggler.getAttribute('aria-expanded') === 'true';
 
     if (expanded) {
         // 開いた → ✖ にする
