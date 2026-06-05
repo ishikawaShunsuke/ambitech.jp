48ca7f89f32cac318d1469ba43608640b968fdf7
ishikawa shunsuke <syantako@gmail.com>
2026-06-05 11:51:30 +0900

    Update common.js

--- public/common.js
+++ public/common.js
@@ -8,9 +8,7 @@ let body =document.querySelector("body");
 const loading = document.querySelector('.loading');
 const nav = document.getElementById('navbarNav');
 const toggler = document.querySelector('.navbar-toggler');
-
-toggler.setAttribute('aria-expanded', 'false');
-
+/*
 nav.addEventListener('show.bs.collapse', () => {
     toggler.disabled = true;
 });
@@ -26,7 +24,7 @@ nav.addEventListener('hide.bs.collapse', () => {
 nav.addEventListener('hidden.bs.collapse', () => {
     toggler.disabled = false;
 });
-
+*/
 //クリックイベントを追加
 scroll_to_top_btn.addEventListener('click', scroll_to_top);
 
